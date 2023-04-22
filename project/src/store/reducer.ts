8d467d9610a489acf7125d34f6e3a7d4b56e0569
poluectov <poluectov@list.ru>
2023-04-22 10:01:26 +0300

    исправил ошибки

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,5 +1,5 @@
 import { createReducer } from '@reduxjs/toolkit';
-import { changeSortType, choiceCity, updateOffers, loadOffers, requireAuthorization,
+import { changeSortType, choiceCity, loadOffers, requireAuthorization,
   setError, setOffersDataLoadingStatus } from './action';
 import { CITIES, AuthorizationStatus } from '../const';
 import { reviews } from '../mocks/reviews';
